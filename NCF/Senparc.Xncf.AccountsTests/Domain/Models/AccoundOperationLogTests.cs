d6b7c072a0c4bac8515ebad430876e521a23da14
ziqian <672699185@qq.com>
2023-11-03 07:41:44 +0800

    创建Account的单元测试

---
+++ NCF/Senparc.Xncf.AccountsTests/Domain/Models/AccoundOperationLogTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Senparc.Xncf.AccountsTests.Domain.Models;
+
+[TestClass]
+public class AccoundOperationLogTests
+{
+    [TestMethod]
+    public void CreateTest()
+    {
+
+    }
+}
---
+++ NCF/Senparc.Xncf.AccountsTests/TestBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Senparc.Xncf.AccountsTests;
+
+public class TestBase
+{
+    public TestBase()
+    {
+        //引用或者初始化代码
+    }
+}
