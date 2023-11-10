94ee69123752bb62b5167459391b4fa647718433
ziqian <672699185@qq.com>
2023-11-10 17:52:27 +0800

    HTTP请求之Flurl1

---
+++ HTTP请求之Flurl/FlurlSamples/FlurlSamples/Program.cs
@@ -0,0 +1,16 @@
+﻿using Flurl;
+
+//var url = "http://www.some-api.com"
+//    .AppendPathSegment("endpoint")
+//    .SetQueryParams(new
+//    {
+//        api_key = "SomeApiKey",
+//        max_results = 20,
+//        q = "Don't worry, I'll get encoded!"
+//    })
+//    .SetFragment("after-hash");
+//Console.WriteLine(url);
+
+Console.WriteLine(Url.Encode("http://foo.com?x=hi there",true)); // includes reserved characters like / and ?
+Console.WriteLine(Url.EncodeIllegalCharacters("http://foo.com?x=hi there", false)); ; // reserved characters aren't touched
+Console.WriteLine(Url.Decode("http%3A%2F%2Ffoo.com%3Fx%3Dhi there", false));
