17154086a1e9c1be7f618cccb5a888a164a0b996
ziqian <672699185@qq.com>
2023-11-13 17:34:05 +0800

    HTTP请求之Flurl2-Test

--- HTTP请求之Flurl/FlurlSamples/FlurlSamples/Program.cs
+++ HTTP请求之Flurl/FlurlSamples/FlurlSamples/Program.cs
@@ -1,16 +1,39 @@
 ﻿using Flurl;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+using Newtonsoft.Json;
 
-//var url = "http://www.some-api.com"
-//    .AppendPathSegment("endpoint")
-//    .SetQueryParams(new
-//    {
-//        api_key = "SomeApiKey",
-//        max_results = 20,
-//        q = "Don't worry, I'll get encoded!"
-//    })
-//    .SetFragment("after-hash");
-//Console.WriteLine(url);
+var url = "http://www.some-api.com"
+    .AppendPathSegment("endpoint")
+    .SetQueryParams(new
+    {
+        api_key = "SomeApiKey",
+        max_results = 20,
+        q = "Don't worry, I'll get encoded!"
+    })
+    .SetFragment("after-hash");
+Console.WriteLine(url);
 
-Console.WriteLine(Url.Encode("http://foo.com?x=hi there",true)); // includes reserved characters like / and ?
+Console.WriteLine(Url.Encode("http://foo.com?x=hi there", true)); // includes reserved characters like / and ?
 Console.WriteLine(Url.EncodeIllegalCharacters("http://foo.com?x=hi there", false)); ; // reserved characters aren't touched
 Console.WriteLine(Url.Decode("http%3A%2F%2Ffoo.com%3Fx%3Dhi there", false));
+
+
+using (var session = new CookieSession("https://cookies.com"))
+{
+    // set any initial cookies on session.Cookies
+
+    await session.Request("a").GetAsync();
+    await session.Request("b").GetAsync();
+    // read cookies at any point using session.Cookies
+}
+
+FlurlHttp.Configure(settings =>
+{
+    var jsonSettings = new JsonSerializerSettings
+    {
+        NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
+        ObjectCreationHandling = ObjectCreationHandling.Replace
+    };
+    settings.JsonSerializer = new NewtonsoftJsonSerializer(jsonSettings);
+});
