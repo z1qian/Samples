f860cc0eed6e08ddde8213b8dc01899871146bec
ziqian <672699185@qq.com>
2023-11-20 09:25:03 +0800

    微信开发MessageHandler 1

---
+++ 微信开发/MessageHandlerPro/MessageHandler/Controllers/WeixinController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.Entities.Request;
+
+namespace MessageHandler.Controllers;
+public class WeixinController : Controller
+{
+    /// <summary>
+    /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://sdk.weixin.senparc.com/weixin
+    /// </summary>
+    [HttpGet]
+    [ActionName("Index")]
+    public ActionResult Get(PostModel postModel, string echostr)
+    {
+        if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, Token))
+        {
+            return Content(echostr); //返回随机字符串则表示验证通过
+        }
+        else
+        {
+            return Content("failed:" + postModel.Signature + "," + CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。" +
+                "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+        }
+    }
+}
---
+++ 微信开发/MessageHandlerPro/SenparcClass.Service/CustomMessageHandler.cs
@@ -0,0 +1,34 @@
+﻿using Senparc.NeuChar.App.AppStore;
+using Senparc.NeuChar.Entities;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MessageHandlers;
+using System.Xml.Linq;
+
+namespace SenparcClass.Service;
+
+/// <summary>
+/// 自定义MessageHandler
+/// 把MessageHandler作为基类，重写对应请求的处理方法
+/// </summary>
+public class CustomMessageHandler : MessageHandler<CustomMessageContext>  /*如果不需要自定义，可以直接使用：MessageHandler<DefaultMpMessageContext> */
+{
+    public CustomMessageHandler(Stream inputStream, PostModel postModel, int maxRecordCount = 0, bool onlyAllowEncryptMessage = false, DeveloperInfo developerInfo = null, IServiceProvider serviceProvider = null) : base(inputStream, postModel, maxRecordCount, onlyAllowEncryptMessage, developerInfo, serviceProvider)
+    {
+    }
+
+    public CustomMessageHandler(XDocument requestDocument, PostModel postModel, int maxRecordCount = 0, bool onlyAllowEncryptMessage = false, DeveloperInfo developerInfo = null, IServiceProvider serviceProvider = null) : base(requestDocument, postModel, maxRecordCount, onlyAllowEncryptMessage, developerInfo, serviceProvider)
+    {
+    }
+
+    public CustomMessageHandler(RequestMessageBase requestMessageBase, PostModel postModel, int maxRecordCount = 0, bool onlyAllowEncryptMessage = false, DeveloperInfo developerInfo = null, IServiceProvider serviceProvider = null) : base(requestMessageBase, postModel, maxRecordCount, onlyAllowEncryptMessage, developerInfo, serviceProvider)
+    {
+    }
+
+    public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+    {
+        var responseMessage = this.CreateResponseMessage<ResponseMessageText>();
+        responseMessage.Content = $"这条消息来自DefaultResponseMessage。\r\n您收到这条消息，表明该公众号没有对【{requestMessage.MsgType}】类型做处理。";
+        return responseMessage;
+    }
+}
