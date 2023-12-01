dde318f9e199e4875bdecf72bada6e9e40899141
ziqian <672699185@qq.com>
2023-12-01 10:01:27 +0800

    创建 接口调用凭证及数据容器 项目

---
+++ 微信开发/接口调用凭证及数据容器（Container）/接口调用凭证及数据容器（Container）/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using 接口调用凭证及数据容器_Container_.Models;
+
+namespace 接口调用凭证及数据容器_Container_.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ 微信开发/接口调用凭证及数据容器（Container）/接口调用凭证及数据容器（Container）/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace 接口调用凭证及数据容器_Container_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
