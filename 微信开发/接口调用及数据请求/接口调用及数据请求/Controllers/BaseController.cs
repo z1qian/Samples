30b1b04b33c8f00e88dd2b67cbfcf5188dbae17b
ziqian <672699185@qq.com>
2023-12-07 07:14:57 +0800

    接口调用及数据请求init

---
+++ 微信开发/接口调用及数据请求/接口调用及数据请求/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senparc.Weixin;
+using Senparc.Weixin.Entities;
+
+namespace 接口调用及数据请求.Controllers;
+public class BaseController : Controller
+{       
+    protected string AppId
+    {
+        get
+        {
+            return Config.SenparcWeixinSetting.WeixinAppId;//与微信公众账号后台的AppId设置保持一致，区分大小写。
+        }
+    }
+
+    protected static ISenparcWeixinSettingForMP MpSetting
+    {
+        get
+        {
+            return Config.SenparcWeixinSetting.MpSetting;
+        }
+    }
+}
