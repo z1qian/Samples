2e9f9adbb79dd88aac75ea3717c1a5c34a837df9
ziqian <672699185@qq.com>
2023-12-08 15:03:33 +0800

    接口调用及数据请求：Post，及Fiddler代理

--- 微信开发/接口调用及数据请求/接口调用及数据请求/Controllers/RequestController.cs
+++ 微信开发/接口调用及数据请求/接口调用及数据请求/Controllers/RequestController.cs
@@ -26,11 +26,32 @@ public class RequestController : BaseController
 
     [HttpGet]
     public async Task<ActionResult> SimulateLogin(string url = "http://www.baidu.com")
-    {     
+    {
         var cookieContainer = new CookieContainer();
         var html = await Senparc.CO2NET.HttpUtility.RequestUtility.HttpGetAsync(_serviceProvider,
             url, cookieContainer, Encoding.UTF8);
 
         return Content(html, "text/html");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Post(string url = "https://sdk.weixin.senparc.com/AsyncMethods/TemplateMessageTest", string code = "")
+    {
+        //设置Fiddler代理
+        //Senparc.CO2NET.HttpUtility.RequestUtility.SetHttpProxy("http://127.0.0.1", "8888", null, null);
+
+        Dictionary<string, string> formData = new Dictionary<string, string>()
+        {
+            ["checkcode"] = code,
+            ["__RequestVerificationToken"] = "CfDJ8HJ8aLs5PrhKoiuWe5Q_4l2C2LYTPsFmVBPS8KcgQ64OIO3zhufBfiM-VDcqfeAx17TEoj3pTT-RF9-7JnmiV592tOUVNaMSdVScTr4LBXwd8IibD5mQLi-SXzoT0gOEFWX1foFc4I5qfyUpC75a65E"
+        };
+        var html = await Senparc.CO2NET.HttpUtility.RequestUtility.HttpPostAsync(_serviceProvider, url,
+            formData: formData, encoding: Encoding.UTF8);
+        html = $"<span style='color:red'>{html}</span>";
+
+        //清除代理
+        //Senparc.CO2NET.HttpUtility.RequestUtility.RemoveHttpProxy();
+
+        return Content(html, "text/html", Encoding.UTF8);
     }
 }
