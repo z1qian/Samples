2917eff6d227160483021bb3f4674e5dc63612b8
ziqian <672699185@qq.com>
2024-01-22 00:04:40 +0800

    SPA与WebAPI的集成1

---
+++ SPA与WebAPI的集成/SPA与WebAPI/SPA与WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPA与WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Index()
+    {
+        return Content("ok");
+    }
+}
---
+++ SPA与WebAPI的集成/SPA与WebAPI/SPA与WebAPI/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSpaStaticFiles(options =>
+{
+    options.RootPath = "ClientApp";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseStaticFiles(new StaticFileOptions()
+//{
+//    RequestPath = new Microsoft.AspNetCore.Http.PathString("/static"),
+//    FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "clientApp/static"))
+//});
+
+app.UseSpaStaticFiles();
+
+app.UseSpa(spa =>
+{
+    //spa.Options.DefaultPage = "/index.html";
+    spa.Options.SourcePath = "ClientApp";
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
