0ddb3691ed514b30142c741daa35c5587c1e8ec7
coren <zzheng2@senparc.com>
2024-02-01 10:54:43 +0800

    H5支付，暂未实现

---
+++ 微信开发/微信支付/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace 微信支付.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult H5Pay(string name)
+        {
+            ViewData["Name"] = name;
+
+            return View();
+        }
+    }
+}
