377dac14344ab53197faa883bf648145e0cfea26
ziqian <672699185@qq.com>
2024-03-11 07:34:21 +0800

    uniapp demo1 暂存

---
+++ uniapp/2.1 demo1/pages/index/index.vue
@@ -0,0 +1,31 @@
+<!-- 以下代码只适合h5页面，因为div，h1，h2，span等只在h5中适配，它们本来就是h5中的标签，在小程序和app中不适配 -->
+<template>
+	<div class="box">
+		<h1>
+			uniapp<span class="row">零基础入门</span>
+		</h1>
+		<view>从基础到进阶</view>
+		<h2>标题二加粗<i>标签</i></h2>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang="scss">
+	.box {
+		// width: 100px;
+		height: 100px;
+		background-color: pink;
+
+		h1 {
+			font-size: 40px;
+			color: red;
+
+			.row {
+				font-style: italic;
+			}
+		}
+	}
+</style>
---
+++ uniapp/2.1 demo1/pages/index2/index2.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="out">
+		<view class="box"></view>
+		<view class="block"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.out{
+	.box{
+		width: 200px;
+		height: 200px;
+		background: pink;
+	}
+	
+	.block{
+		width: 750rpx;
+		height: 200rpx;
+		background: green;
+	}
+}
+</style>
