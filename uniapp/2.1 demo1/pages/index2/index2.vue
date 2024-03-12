77c60eba7d67cbaa9ee8e3a0e09e2d5c8c1b91d0
ziqian <672699185@qq.com>
2024-03-12 11:58:48 +0800

    uniapp ico,view,text组件的使用

--- uniapp/2.1 demo1/pages/index2/index2.vue
+++ uniapp/2.1 demo1/pages/index2/index2.vue
@@ -2,6 +2,12 @@
 	<view class="out">
 		<view class="box"></view>
 		<view class="block"></view>
+		<icon type="waiting" size="26" />
+		<icon type="success" size="26" />
+		<view>uniapp</view>
+		<view>web前端开发</view>
+		<text selectable user-select>nodejs span</text>
+		<text space="emsp" decode>vue    &gt;        js</text>
 	</view>
 </template>
 
@@ -9,24 +15,24 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
-.out{
-	.box{
-		width: 200px;
-		height: 200px;
-		background: pink;
+	.out {
+		.box {
+			width: 200px;
+			height: 200px;
+			background: pink;
+		}
+
+		.block {
+			width: 750rpx;
+			height: 200rpx;
+			background: green;
+		}
 	}
-	
-	.block{
-		width: 750rpx;
-		height: 200rpx;
-		background: green;
-	}
-}
 </style>
