3db463b1c7ac829bf9080f00b3656f6027b60f12
ziqian <672699185@qq.com>
2024-03-13 07:42:50 +0800

    swiper 暂存

--- uniapp/2.1 demo1/pages/index2/index2.vue
+++ uniapp/2.1 demo1/pages/index2/index2.vue
@@ -7,7 +7,43 @@
 		<view>uniapp</view>
 		<view>web前端开发</view>
 		<text selectable user-select>nodejs span</text>
-		<text space="emsp" decode>vue    &gt;        js</text>
+		<text space="emsp" decode>vue &gt; js</text>
+
+		<scroll-view class="scroll" scroll-x scroll-y>
+			<view class="group">
+				<view class="item">
+					111
+				</view>
+
+				<view class="item">
+					222
+				</view>
+
+				<view class="item">
+					333
+				</view>
+
+				<view class="item">
+					444
+				</view>
+
+				<view class="item">
+					555
+				</view>
+			</view>
+		</scroll-view>
+		
+		<swiper class="swiper" >
+			<swiper-item  class="item">
+				<image src="../../static/images/pic1.jpg"></image>
+			</swiper-item>
+			<swiper-item  class="item">
+				<image src="../../static/images/pic2.jpg"></image>
+			</swiper-item>
+			<swiper-item  class="item">
+				<image src="../../static/images/pic3.png"></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -33,6 +69,24 @@
 			width: 750rpx;
 			height: 200rpx;
 			background: green;
+		}
+	}
+
+	.scroll {
+		border: 1px solid red;
+		box-sizing: border-box;
+		height: 100rpx;
+
+		.group {
+			white-space: nowrap;
+
+			.item {
+				width: 220rpx;
+				height: 220rpx;
+				background: royalblue;
+				display: inline-block;
+				margin-right: 10rpx;
+			}
 		}
 	}
 </style>
