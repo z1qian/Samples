452f575a022e7543a62bc996fd19a0b3cd76fc5b
ziqian <672699185@qq.com>
2024-03-18 07:32:27 +0800

    媒体组件暂存

---
+++ uniapp/2.1 demo1/pages/medium/medium.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<video src="/static/medium/dd48ef4d0a0cbfc432ca832029e155dd.mp4" muted initial-time=2
+			:danmu-btn="false" show-mute-btn></video>
+		<audio src="https://web-ext-storage.dcloud.net.cn/uni-app/ForElise.mp3"></audio>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
